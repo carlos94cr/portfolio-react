ee8667d0ff7c6e7f9b29b8534d1304a5d1168e0a
carlos94cr <carlos94cr@gmail.com>
2022-06-19 20:20:30 +0200

    add fixed social icons

--- src/components/SocialIcon/SocialIcon.js
+++ src/components/SocialIcon/SocialIcon.js
@@ -37,7 +37,7 @@ function SocialIcon() {
       <ul>
         <li className="item">
           <a
-            href="https://www.linkedin.com/in/your_username/"
+            href="https://es.linkedin.com/in/carlos-de-la-cruz-romero-2a68621a3/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -46,7 +46,7 @@ function SocialIcon() {
         </li>
         <li className="item">
           <a
-            href="https://github.com/your_username/"
+            href="https://www.github.com/carlos94cr/"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -55,7 +55,7 @@ function SocialIcon() {
         </li>
         <li className="item">
           <a
-            href="https://github.com/your_username/"
+            href="https://www.instagram.com/carloscruz_photography/"
             target="_blank"
             rel="noopener noreferrer"
           >
